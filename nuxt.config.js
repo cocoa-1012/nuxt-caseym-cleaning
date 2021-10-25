929c47a48e7c60ecbf4ebd9736ccbf1504ffcf51
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-10-25 15:09:29 -0400

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'caseym',
+    title: 'Gabe A Duhon - Attorneys At Law',
     htmlAttrs: {
       lang: 'en'
     },
