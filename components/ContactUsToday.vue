405a358a4779edfaeb27b1b16dce40186f05aeea
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-10-26 13:39:31 -0400

    clean up contact us today section

--- components/ContactUsToday.vue
+++ components/ContactUsToday.vue
@@ -2,21 +2,84 @@
   <div class="contact-section about-contact wf-section">
     <div class="contact-container about-contact-container w-container">
       <div class="w-row">
-        <div class="contact-table w-col w-col-4"><img src="assets/images/aboutimage1.bd629355.png" loading="lazy" alt="">
-          <p class="contact-section-text about">Contact Us <br>Today!<br></p>
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage2.839e161e.png"
+            loading="lazy"
+            alt=""
+          />
         </div>
-        <div class="contact-table w-col w-col-4"><img src="assets/images/aboutimage1.bd629355.png" loading="lazy" alt="">
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage1.bd629355.png"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage2.839e161e.png"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-row">
+        <div class="contact-table w-col w-col-4">
+          <p class="contact-section-text about">
+            Contact Us <br />Today!<br />
+          </p>
+        </div>
+        <div class="contact-table w-col w-col-8">
           <div class="w-form">
             <form id="email-form" name="email-form" data-name="Email Form">
               <div class="w-row">
                 <div class="contact-form-column-two w-col w-col-6">
-                  <input id="name" type="text" class="w-input" maxlength="256" name="name" data-name="Name" placeholder="Name">
-                  <input id="email" type="email" class="w-input" maxlength="256" name="email" data-name="Email" placeholder="Email" required="">
-                  <input id="Phone" type="tel" class="w-input" maxlength="256" name="Phone" data-name="Phone" placeholder="Phone" required="">
+                  <input
+                    id="name"
+                    type="text"
+                    class="w-input"
+                    maxlength="256"
+                    name="name"
+                    data-name="Name"
+                    placeholder="Name"
+                  />
+                  <input
+                    id="email"
+                    type="email"
+                    class="w-input"
+                    maxlength="256"
+                    name="email"
+                    data-name="Email"
+                    placeholder="Email"
+                    required=""
+                  />
+                  <input
+                    id="Phone"
+                    type="tel"
+                    class="w-input"
+                    maxlength="256"
+                    name="Phone"
+                    data-name="Phone"
+                    placeholder="Phone"
+                    required=""
+                  />
                 </div>
                 <div id="field" class="contact-form-column-two w-col w-col-6">
-                  <textarea placeholder="Example Text" maxlength="5000" name="field" data-name="Field" class="textarea w-input"></textarea>
-                  <input type="submit" value="Submit" data-wait="Please wait..." class="button contact w-button">
+                  <textarea
+                    placeholder="Example Text"
+                    maxlength="5000"
+                    rows="3"
+                    name="field"
+                    data-name="Field"
+                    class="textarea w-input"
+                  ></textarea>
+                  <input
+                    type="submit"
+                    value="Submit"
+                    data-wait="Please wait..."
+                    class="button contact w-button today-button"
+                  />
                 </div>
               </div>
             </form>
@@ -28,10 +91,7 @@
             </div>
           </div>
         </div>
-        <div class="contact-table w-col w-col-4"><img src="assets/images/aboutimage1.bd629355.png" loading="lazy" alt=""></div>
       </div>
     </div>
   </div>
-  
-  
 </template>
