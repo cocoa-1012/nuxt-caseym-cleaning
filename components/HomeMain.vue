275d7f413afe6950a968e6d40b95ff2256f300bf
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-10-26 14:01:45 -0400

    homepage contact us section

--- components/HomeMain.vue
+++ components/HomeMain.vue
@@ -2,10 +2,12 @@
   <div>
     <div class="page-hero home wf-section">
       <div class="overlay-section w-container">
-        <h1 class="hero-text">Experienced and Local<br>Attorneys Here for You</h1>
+        <h1 class="hero-text">
+          Experienced and Local<br />Attorneys Here for You
+        </h1>
       </div>
     </div>
-    <HomeCarousel/>
+    <HomeCarousel />
     <div class="why-choose-us wf-section">
       <div class="wcu-images w-row">
         <div class="column-6 w-col w-col-4"></div>
@@ -17,16 +19,43 @@
     <div class="about-grey wf-section">
       <div class="about-grey-text-section w-row">
         <div class="w-col w-col-6 w-col-medium-6">
-          <p class="about-grey-text">At the law office of Gabe A. Duhon, LLC, attorneys Gabe A. Duhon and Wyman E. Bankston are proud to serve the citizens of Abbeville, Livingston and the rest of Acadiana with personalized local service since 2006.<strong><br></strong></p>
-          <a href="about.html" class="button w-button"><strong>MEET OUR ATTORNEYS</strong></a>
+          <p class="about-grey-text">
+            At the law office of Gabe A. Duhon, LLC, attorneys Gabe A. Duhon and
+            Wyman E. Bankston are proud to serve the citizens of Abbeville,
+            Livingston and the rest of Acadiana with personalized local service
+            since 2006.<strong><br /></strong>
+          </p>
+          <a href="about.html" class="button w-button"
+            ><strong>MEET OUR ATTORNEYS</strong></a
+          >
         </div>
-        <div class="column-7 w-col w-col-3 w-col-medium-3"><img src="assets/images/person1.046db6ca.png" loading="lazy" alt="" class="circle-image">
-          <p class="about-grey-text title">Gabe A. Duhon<strong><br></strong></p>
-          <p class="about-grey-text title sub-title">attorney/owner<strong><br></strong></p>
+        <div class="column-7 w-col w-col-3 w-col-medium-3">
+          <img
+            src="assets/images/person1.046db6ca.png"
+            loading="lazy"
+            alt=""
+            class="circle-image"
+          />
+          <p class="about-grey-text title">
+            Gabe A. Duhon<strong><br /></strong>
+          </p>
+          <p class="about-grey-text title sub-title">
+            attorney/owner<strong><br /></strong>
+          </p>
         </div>
-        <div class="column-9 w-col w-col-3 w-col-medium-3"><img src="assets/images/person2.b2145e19.png" loading="lazy" alt="" class="circle-image">
-          <p class="about-grey-text title">Kimberly K. Souriyakhamphong<strong><br></strong></p>
-          <p class="about-grey-text title sub-title">associate attorney<strong><br></strong></p>
+        <div class="column-9 w-col w-col-3 w-col-medium-3">
+          <img
+            src="assets/images/person2.b2145e19.png"
+            loading="lazy"
+            alt=""
+            class="circle-image"
+          />
+          <p class="about-grey-text title">
+            Kimberly K. Souriyakhamphong<strong><br /></strong>
+          </p>
+          <p class="about-grey-text title sub-title">
+            associate attorney<strong><br /></strong>
+          </p>
         </div>
       </div>
     </div>
@@ -34,21 +63,76 @@
       <div class="contact-container w-container">
         <div class="w-row">
           <div class="w-col w-col-4">
-            <p class="contact-section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam cursus metus, at accumsan massa ornare id. Aliquam facilisis ex aliquet, lacinia nunc at, semper nunc. Vivamus vel urna nec quam elementum ultricies. Vivamus sed sem venenatis nibh lacinia imperdiet sit amet sit amet ex. Nam vehicula venenatis odio, id feugiat libero gravida facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br></p>
+            <p class="contact-section-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              aliquam cursus metus, at accumsan massa ornare id. Aliquam
+              facilisis ex aliquet, lacinia nunc at, semper nunc. Vivamus vel
+              urna nec quam elementum ultricies. Vivamus sed sem venenatis nibh
+              lacinia imperdiet sit amet sit amet ex. Nam vehicula venenatis
+              odio, id feugiat libero gravida facilisis. Class aptent taciti
+              sociosqu ad litora torquent per conubia nostra, per inceptos
+              himenaeos. <br />
+            </p>
           </div>
           <div class="w-col w-col-8">
-            <p class="contact-us-title">Contact Us<strong><br></strong></p>
+            <p class="contact-us-title">
+              Contact Us<strong><br /></strong>
+            </p>
             <div class="w-form">
               <form id="email-form" name="email-form" data-name="Email Form">
                 <div class="w-row">
-                  <div class="contact-form-column-two w-col w-col-6">
-                    <input id="name" type="text" class="w-input" maxlength="256" name="name" data-name="Name" placeholder="Name">
-                    <input id="email" type="email" class="w-input" maxlength="256" name="email" data-name="Email" placeholder="Email" required="">
-                    <input id="Phone" type="tel" class="w-input" maxlength="256" name="Phone" data-name="Phone" placeholder="Phone" required="">
+                  <div
+                    class="
+                      contact-form-column-two
+                      w-col w-col-6
+                      contact-us-group
+                    "
+                  >
+                    <input
+                      id="name"
+                      type="text"
+                      class="w-input"
+                      maxlength="256"
+                      name="name"
+                      data-name="Name"
+                      placeholder="Name"
+                    />
+                    <input
+                      id="email"
+                      type="email"
+                      class="w-input"
+                      maxlength="256"
+                      name="email"
+                      data-name="Email"
+                      placeholder="Email"
+                      required=""
+                    />
+                    <input
+                      id="Phone"
+                      type="tel"
+                      class="w-input"
+                      maxlength="256"
+                      name="Phone"
+                      data-name="Phone"
+                      placeholder="Phone"
+                      required=""
+                    />
                   </div>
                   <div class="contact-form-column-two w-col w-col-6">
-                    <textarea id="field" placeholder="Example Text" maxlength="5000" name="field" class="textarea w-input"></textarea>
-                    <input type="submit" value="Submit" data-wait="Please wait..." class="button contact w-button">
+                    <textarea
+                      id="field"
+                      placeholder="Example Text"
+                      maxlength="5000"
+                      name="field"
+                      rows="6"
+                      class="textarea w-input"
+                    ></textarea>
+                    <input
+                      type="submit"
+                      value="Submit"
+                      data-wait="Please wait..."
+                      class="button contact w-button"
+                    />
                   </div>
                 </div>
               </form>
@@ -66,12 +150,34 @@
     <div class="real-estate-title-services-section wf-section">
       <div class="workingwithtextsection real-estate-title-services w-row">
         <div class="w-col w-col-6">
-          <h1 class="working-with-header"><em>Our Real <br>Estate Title <br>Services</em></h1>
+          <h1 class="working-with-header">
+            <em>Our Real <br />Estate Title <br />Services</em>
+          </h1>
         </div>
         <div class="w-col w-col-6">
-          <p class="working-with-subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam cursus metus, at accumsan massa ornare id. Aliquam facilisis ex aliquet, lacinia nunc at, semper nunc. Vivamus vel urna nec quam elementum ultricies. Vivamus sed sem venenatis nibh lacinia imperdiet sit amet sit amet ex. Nam vehicula venenatis odio, id feugiat libero gravida facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec commodo ex vitae odio tincidunt sodales vel at massa. Nulla facilisi. Etiam cursus varius urna, vel mattis sapien venenatis at. Donec et eros eros. Phasellus arcu mi, hendrerit eu nibh eu, condimentum iaculis sapien. <br><br>Donec eu blandit elit, in ultrices tellus. Maecenas ultricies bibendum sodales. Morbi in tincidunt eros. Vivamus nec ipsum suscipit, sagittis libero eget, vehicula leo. Aliquam erat volutpat. Donec auctor luctus diam, id finibus neque pellentesque vitae. Duis pretium ut velit quis cursus. Aliquam vitae leo nec neque luctus gravida. Nulla vestibulum, felis nec blandit lobortis, neque nisi aliquam nunc, eget vehicula lectus augue interdum leo. Nam vestibulum a sem id ornare. Praesent convallis mollis elementum.<br></p>
+          <p class="working-with-subtext">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            aliquam cursus metus, at accumsan massa ornare id. Aliquam facilisis
+            ex aliquet, lacinia nunc at, semper nunc. Vivamus vel urna nec quam
+            elementum ultricies. Vivamus sed sem venenatis nibh lacinia
+            imperdiet sit amet sit amet ex. Nam vehicula venenatis odio, id
+            feugiat libero gravida facilisis. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos. Donec
+            commodo ex vitae odio tincidunt sodales vel at massa. Nulla
+            facilisi. Etiam cursus varius urna, vel mattis sapien venenatis at.
+            Donec et eros eros. Phasellus arcu mi, hendrerit eu nibh eu,
+            condimentum iaculis sapien. <br /><br />Donec eu blandit elit, in
+            ultrices tellus. Maecenas ultricies bibendum sodales. Morbi in
+            tincidunt eros. Vivamus nec ipsum suscipit, sagittis libero eget,
+            vehicula leo. Aliquam erat volutpat. Donec auctor luctus diam, id
+            finibus neque pellentesque vitae. Duis pretium ut velit quis cursus.
+            Aliquam vitae leo nec neque luctus gravida. Nulla vestibulum, felis
+            nec blandit lobortis, neque nisi aliquam nunc, eget vehicula lectus
+            augue interdum leo. Nam vestibulum a sem id ornare. Praesent
+            convallis mollis elementum.<br />
+          </p>
         </div>
       </div>
     </div>
-  </div>  
+  </div>
 </template>
