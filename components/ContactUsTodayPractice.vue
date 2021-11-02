42d9776480343c914e6a1017b94636a248cbcce3
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-02 16:03:59 -0400

    practice area page update

---
+++ components/ContactUsTodayPractice.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    class="contact-section about-contact wf-section practice"
+    style="background-color: #892432; margin-top: 10px"
+  >
+    <div
+      class="contact-container about-contact-container w-container"
+      style="background: #892432; padding-top: 0px"
+    >
+      <div class="w-row">
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage2.839e161e.png"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage1.bd629355.png"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+        <div class="contact-table w-col w-col-4">
+          <img
+            src="assets/images/aboutimage2.839e161e.png"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-row">
+        <div class="contact-table w-col w-col-4">
+          <p class="contact-section-text about" style="color: white">
+            Contact Us <br />Today!<br />
+          </p>
+        </div>
+        <div class="contact-table w-col w-col-8">
+          <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form">
+              <div class="w-row">
+                <div class="contact-form-column-two w-col w-col-6">
+                  <input
+                    id="name"
+                    type="text"
+                    class="w-input"
+                    maxlength="256"
+                    name="name"
+                    data-name="Name"
+                    placeholder="NAME"
+                  />
+                  <input
+                    id="email"
+                    type="email"
+                    class="w-input"
+                    maxlength="256"
+                    name="email"
+                    data-name="Email"
+                    placeholder="EMAIL"
+                    required=""
+                  />
+                  <input
+                    id="Phone"
+                    type="tel"
+                    class="w-input"
+                    maxlength="256"
+                    name="Phone"
+                    data-name="Phone"
+                    placeholder="PHONE"
+                    required=""
+                  />
+                </div>
+                <div id="field" class="contact-form-column-two w-col w-col-6">
+                  <textarea
+                    placeholder="MESSAGE"
+                    maxlength="5000"
+                    rows="6"
+                    name="field"
+                    data-name="Field"
+                    class="textarea w-input"
+                  ></textarea>
+                  <input
+                    type="submit"
+                    value="Submit"
+                    data-wait="Please wait..."
+                    class="button contact w-button today-button"
+                    style="background: #fff; color: #4f748c"
+                  />
+                </div>
+              </div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/PracticeCarousel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="working-with-section wf-section">
+    <div
+      data-current="WORKING WITH HOME BUYERS"
+      data-easing="ease-in-out"
+      data-duration-in="300"
+      data-duration-out="100"
+      class="tabs w-tabs"
+      style="width: 80%"
+    >
+      <div class="tabs-menu w-tab-menu home-carousel practice">
+        <a
+          id="carousel-item0"
+          data-w-tab="WORKING WITH HOME BUYERS"
+          class="
+            tab-link-working-with-practice-buyers
+            w-inline-block w-tab-link w--current
+          "
+          @click="showPage(0)"
+        >
+          <div class="tabs-heading">
+            WORKING WITH <strong>HOME BUYERS</strong>
+          </div>
+        </a>
+        <a
+          id="carousel-item1"
+          data-w-tab="WORKING WITH LENDERS"
+          class="
+            tab-link-working-with-practice-buyers
+            w-inline-block w-tab-link
+          "
+          @click="showPage(1)"
+        >
+          <div class="text-block-2 tabs-heading">
+            WORKING WITH <strong>LENDERS</strong>
+          </div>
+        </a>
+        <a
+          id="carousel-item2"
+          data-w-tab="WORKING WITH REALTORS"
+          class="
+            tab-link-working-with-practice-buyers
+            w-inline-block w-tab-link
+          "
+          @click="showPage(2)"
+        >
+          <div class="tabs-heading">WORKING WITH <strong>REALTORS</strong></div>
+        </a>
+      </div>
+      <div class="carousel-main">
+        <VueSlickCarousel
+          ref="carousel"
+          class="w-tab-content"
+          :fade="true"
+          :draggable="false"
+        >
+          <PracticeCarouselSection />
+          <PracticeCarouselSection />
+          <PracticeCarouselSection />
+        </VueSlickCarousel>
+        <div class="carousel-overlay" style="background-color: #707070"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: 'Carousel',
+  components: { VueSlickCarousel },
+  methods: {
+    showPage(index) {
+      this.$refs.carousel.goTo(index)
+      if (index === 0) {
+        document.getElementById('carousel-item0').classList.add('w--current')
+        document.getElementById('carousel-item1').classList.remove('w--current')
+        document.getElementById('carousel-item2').classList.remove('w--current')
+      } else if (index === 1) {
+        document.getElementById('carousel-item1').classList.add('w--current')
+        document.getElementById('carousel-item0').classList.remove('w--current')
+        document.getElementById('carousel-item2').classList.remove('w--current')
+      }
+      if (index === 2) {
+        document.getElementById('carousel-item2').classList.add('w--current')
+        document.getElementById('carousel-item1').classList.remove('w--current')
+        document.getElementById('carousel-item0').classList.remove('w--current')
+      }
+    },
+  },
+}
+</script> 
