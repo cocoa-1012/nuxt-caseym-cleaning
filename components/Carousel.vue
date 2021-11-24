4226df3f554ea6bda4a7568c93232116b775b746
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-23 21:36:07 -0500

    testimonial carousel update hidden scroll

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -34,12 +34,11 @@
                 Proin eu pulvinar leo. Quisque tempor aliquet convallis. Ut in
                 urna lacinia, viverra ex quis, suscipit sapien. Donec sed ipsum
                 velit. Praesent at faucibus purus. Curabitur pretium nibh vel
-                nisi efficitur viverra. Praesent at faucibus purus. Curabitur
-                pretium nibh vel nisi efficitur viverra. Nullam finibus in
-                lectus ut fermentum. Phasellus hendrerit nec nulla a posuere.
-                Fusce rhoncus ut sem et lobortis. Aenean tempus ut neque eu
-                eleifend. Curabitur quis urna mi. Suspendisse odio justo,
-                lobortis vel auctor ut, ornare eget tortor.<br />LOREM IPSUM
+                nisi efficitur viverra. Nullam finibus in lectus ut fermentum.
+                Phasellus hendrerit nec nulla a posuere. Fusce rhoncus ut sem et
+                lobortis. Aenean tempus ut neque eu eleifend. Curabitur quis
+                urna mi. Suspendisse odio justo, lobortis vel auctor ut, ornare
+                eget tortor.<br />LOREM IPSUM <br /><br />
               </div>
             </div>
           </VueSlickCarousel>
